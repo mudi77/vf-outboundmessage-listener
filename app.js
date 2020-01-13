69c70e735f2e4d35171cd9b1bea4b0387bfbed7c
Igor Collak <colls.ems@gmail.com>
2020-01-13 17:32:08 +0100

    3

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ let port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {    
     res.render('index');
+});
+
+app.post("/", function (req, res) {
+    console.log(req.body);
+    res.send("POST request reseived !!!");
 });
 
 app.listen(port, function() {
