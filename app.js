4620de144b39f57b188d021273c4689af0b3b11b
Igor Collak <colls.ems@gmail.com>
2020-01-13 15:17:26 +0100

    initial comm

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+// set the port of our application
+let port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {    
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
