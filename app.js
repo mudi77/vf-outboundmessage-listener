3cff67219eb8dfb30cdcabc775b6a4c1269685f3
Igor Collak <colls.ems@gmail.com>
2020-01-14 12:05:14 +0100

    13

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let parseXml = require('./parseXml');
 // response.set('Content-Type', 'text/xml');
 // response.send(xml(name_of_restaurants));
 
+let xmlparser = require('express-xml-bodyparser');
+
 // set the port of our application
 let port = process.env.PORT || 8080;
 
@@ -31,18 +33,36 @@ app.get('/', function(req, res) {
     // });
 });
 
-app.post("/", function (req, res) {
-    res.type('application/xml');
 
-    console.log("POST request received !!!", req.body);
+app.post('/', xmlparser(), (req, res) => {
+    console.log('Incoming message!');
+    let xml = `<?xml version="1.0" encoding="utf-8"?>
+  <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <soapenv:Body>
+      <notifications xmlns="http://soap.sforce.com/2005/09/outbound">
+          <Ack>true</Ack>
+      </notifications>
+    </soapenv:Body>
+  </soapenv:Envelope>`;
+
+    console.log("req.body: ", req.body);
+    console.log("xml: ", xml);
+
+    res.set("Content-Type", "text/xml").send(xml);
+  });
+
+//app.post("/", function (req, res) {
+//    res.type('application/xml');
+
+//    console.log("POST request received !!!", req.body);
     //bodyParser(req.body);
 
-    res.render('index', { data: JSON.stringify( req.body ) });
+//    res.render('index', { data: JSON.stringify( req.body ) });
 
     // parseXml.parse(req.body, (data) => {
     //     console.log("data: ", data);
     // });
-});
+//});
 
 app.listen(port, function() {
     console.log('app is running on http://localhost:' + port);
