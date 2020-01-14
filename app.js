598382f0dc4c7694da4736f13d177a6c5f5fa85c
Igor Collak <colls.ems@gmail.com>
2020-01-14 09:42:12 +0100

    5

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+let parseXml = require('./parseXml');
 
 // set the port of our application
 let port = process.env.PORT || 8080;
@@ -7,17 +9,25 @@ let port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+app.use( bodyParser() );
+
 app.get('/', function(req, res) {    
     console.log("GET request reseived !!!", req.body);
     res.render('index');
 });
 
 app.post("/", function (req, res) {
-    console.log("POST request reseived !!!", req.body);
-    res.send("POST request reseived !!!");
+    console.log("POST request received !!!", req.body);
+    //parser(req.body);
+
+    parseXml.parse(req.body, (data) => {
+
+        console.log("data: ", data);
+
+    });
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('app is running on http://localhost:' + port);
 });
 
