81806fbdfdd3ced10c7ce459256354debd3f1649
Igor Collak <colls.ems@gmail.com>
2020-01-23 22:02:01 +0100

    54

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-let parseXml = require('./parseXml');
-let xmlparser = require('express-xml-bodyparser');
+//let parseXml = require('./parseXml');
+//let xmlparser = require('express-xml-bodyparser');
 let path = require('path');
 
+let renderLog = require('./renderLog');
 
-// set the port of our application
 let port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
@@ -16,37 +16,36 @@ app.use(bodyParser.json());
 
 let resultData = {"key1":"val1","key2":"val2","key3":"val3"};
 
-app.get('/', function(req, res) {    
-    console.log("GET request !", req.body);
-    res.render('index', { data: JSON.stringify( resultData, undefined, 4 )  });    
+
+
+//READ DATA
+app.get('/', async function(req, res) {    
+    //let updatedData = await renderLog.update.getNew();
+    res.render('index', { data: "" });    
 });
 
+
+//SAVE DATA
 app.post('/sf-trgr', function(req, res) {    
     console.log("DATA received! - Account " + (req.body.isInsert == 'yes' ? 'Inserted' : 'Updated') + " ID: " + req.body.Id);
 
-
+    renderLog.update.setNew( req.body );
     res.render('index', { data: JSON.stringify( resultData, undefined, 4 )  });    
 });
 
-app.post('/', xmlparser(), (req, res, next) => {
-    console.log('SF outbound msg received! \n');
 
-    let processMsg = {};    
+//READ DATA
+app.get('/render', async (req, res) => {
+    let updatedData = await renderLog.update.getNew();
 
-    try{
-        resultData = parseXml.utils.parse(req.body);
-        processMsg = parseXml.utils.parse(req.body);
+    console.log('updatedData: ', updatedData );
+    res.send( updatedData );
+});
 
-        console.log("processMsg: ", processMsg);
 
-        res.render('index', { data: JSON.stringify( resultData, undefined, 4 )  });  
-    }catch(e){
 
-        console.log("e: ", e);
-        res.render('index', { data: JSON.stringify( resultData, undefined, 4 )  });  
-    }
 
-  });
+
 
 app.listen(port, function() {
     console.log('app is running on http://localhost:' + port);
